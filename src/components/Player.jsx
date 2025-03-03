9fca8874dfc179e1ca73ad92ee2622a8eaaccd0d
Devansh Sharma <sharmadevansh007777@gmail.com>
2025-03-03 21:43:55 +0100

    Added player names and made them editable using two way binding of react useState

---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEdit() {
+    // we use a function to update the state based on the previous state
+    // this is important when the new state depends on the previous state
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let buttonCaption = "Edit";
+
+    // two way binding in <input>
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    buttonCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>{buttonCaption}</button>
+    </li>
+  );
+}
